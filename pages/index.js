493dcdce3f98f6b39f36acbe1c820d5d84aa6e53
EGFellow <93233671+EGFellow@users.noreply.github.com>
2022-05-08 22:11:30 -0400

    Title and description change

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Ananke Global" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The Future of Gaming
         </p>
       </main>
 
